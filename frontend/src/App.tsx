b0c5cd5e064b6c6693104f45b950dd0b525f67c2
John Yuan <yuan411@purdue.edu>
2026-08-26 17:56:49 -0400

    Init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2 className="font-bold text-xl">Hello world</h2>
+      <div>CS407 project</div>
+    </>
+  )
+}
+
+export default App
