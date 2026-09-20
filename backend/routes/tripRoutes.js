4f07ab9bac609b97cd8e59666994d10810de2827
DanielChoi45510 <143205103+DanielChoi45510@users.noreply.github.com>
2026-09-20 16:36:36 -0400

    feat: add delete trip functionality (#9)

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { createTrip, getTrips } = require("../controllers/tripController");
+const { createTrip, getTrips, deleteTrip } = require("../controllers/tripController");
 
-router.post("/", createTrip);
-router.get("/", getTrips);
+router.post('/', createTrip);
+router.get('/', getTrips);
+router.delete('/:id', deleteTrip);
 
 module.exports = router;
